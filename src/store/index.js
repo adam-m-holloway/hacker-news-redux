27277f6c4c199a5228d10ca87c512f58c1a55f48
Adam Holloway <adam.holloway21@gmail.com>
2019-10-27 11:46:39 +0000

    add fetch of HN data and add Sagas

--- src/store/index.js
+++ src/store/index.js
@@ -1,13 +1,19 @@
 import { createStore, applyMiddleware } from 'redux';
 import { createLogger } from 'redux-logger';
+import createSagaMiddleware from 'redux-saga';
 import { composeWithDevTools } from 'redux-devtools-extension';
+import rootSaga from '../sagas';
 import rootReducer from '../reducers';
 
 const logger = createLogger();
+const saga = createSagaMiddleware();
 
 const store = createStore(
   rootReducer,
-  composeWithDevTools(applyMiddleware(logger))
+  undefined,
+  composeWithDevTools(applyMiddleware(saga, logger))
 );
+
+saga.run(rootSaga);
 
 export default store;
